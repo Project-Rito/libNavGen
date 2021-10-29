9b472cf4e90b7cd72d8a5e1864b92b3a7aae7a53
Martin Kröner <kronerm9@gmail.com>
2021-10-29 14:02:04 +0200

    include mingw dlls

--- compile.sh
+++ compile.sh
@@ -13,3 +13,8 @@ cmake -S . -B ./build/ -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchains/mingw-linux.cma
 	cp ./build/libNavGen.dll ./compiled/
 
 rm -rf ./build/
+
+cp /usr/x86_64-w64-mingw32/bin/libgcc_s_seh-1.dll ./compiled/
+cp /usr/x86_64-w64-mingw32/bin/libgomp-1.dll ./compiled/
+cp /usr/x86_64-w64-mingw32/bin/libstdc++-6.dll ./compiled/
+cp /usr/x86_64-w64-mingw32/bin/libwinpthread-1.dll ./compiled/
