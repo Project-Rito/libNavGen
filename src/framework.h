385e5c1d81d86417b72eca31b4f6f6b35beee7da
S41L0R <60116462+S41L0R@users.noreply.github.com>
2022-07-05 13:23:41 -0400

    Fix compilation on windows

--- src/framework.h
+++ src/framework.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define NOMINMAX // Get rid of windows' annoying min and max definitions
+
 #ifdef _WIN32
     #define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
     // Windows Header Files
