3c1be58a2c5ca0db9bde8113b39c41544e9a490a
S41L0R <60116462+S41L0R@users.noreply.github.com>
2022-12-27 13:41:51 -0500

    ugh no work

--- src/dllmain (2).cpp
+++ src/dllmain (2).cpp
@@ -1,9 +1,10 @@
+/*
+
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "Recast.h"
 #include "framework.h"
 #include <cmath>
 #include <cstring>
-#include <vector>
 
 #ifdef _WIN32
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -23,283 +24,33 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 }
 #endif
 
-rcContext* context = nullptr;
-
-rcConfig config;
-DllExport [[maybe_unused]] void
-SetNavmeshBuildParams(float cs, float ch, float walkableSlopeAngle, float walkableHeight, float walkableClimb,
-    float walkableRadius, int minRegionArea) {
-    memset(&config, 0, sizeof(config));
-    config.cs = cs;
-    config.ch = ch;
-    config.walkableSlopeAngle = walkableSlopeAngle;
-    config.walkableHeight = (int)ceilf(walkableHeight / ch);
-    config.walkableClimb = (int)floorf(walkableClimb / ch);
-    config.walkableRadius = (int)ceilf(walkableRadius / cs);
-    config.maxEdgeLen = (int)12;
-    config.maxSimplificationError = 1.3f;
-    config.minRegionArea = (int)rcSqr(minRegionArea); // Note: area = size*size
-    config.mergeRegionArea = (int)rcSqr(20); // Note: area = size*size
-    config.maxVertsPerPoly = 3;
-    config.detailSampleDist = 6;
-    config.detailSampleMaxError = 1;
-}
-
-std::vector<rcPolyMesh*> overlappingTiles;
-DllExport [[maybe_unused]] bool AddTileForMesh(float* verts, int vcount, int* indices, int icount) {
-    if (context == nullptr) {
-        context = new rcContext();
-    }
-
-    float bmin[3];
-    float bmax[3];
-
-    // Calculate bounding box
-    for (int i = 0; i < vcount; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (i == 0) {
-                bmin[j] = verts[j];
-                bmax[j] = verts[j];
-            }
-            else {
-                float v = verts[i * 3 + j];
-                if (v < bmin[j]) {
-                    bmin[j] = v;
-                }
-                if (v > bmax[j]) {
-                    bmax[j] = v;
-                }
-            }
-        }
-    }
-
-    rcVcopy(config.bmin, bmin);
-    rcVcopy(config.bmax, bmax);
-    rcCalcGridSize(config.bmin, config.bmax, config.cs, &config.width, &config.height);
-
-    // Step 2: rasterization
-    rcHeightfield* heightfield = rcAllocHeightfield();
-    if (!heightfield) {
-        return false;
-    }
-    if (!rcCreateHeightfield(context, *heightfield, config.width, config.height, config.bmin, config.bmax, config.cs,
-        config.ch)) {
-        return false;
-    }
-
-    auto* triareas = new unsigned char[icount / 3];
-    memset(triareas, 0, (icount / 3) * sizeof(unsigned char));
-    rcMarkWalkableTriangles(context, config.walkableSlopeAngle, verts, vcount, indices, icount / 3, triareas);
-    if (!rcRasterizeTriangles(context, verts, vcount, indices, triareas, icount / 3, *heightfield,
-        config.walkableClimb)) {
-        return false;
-    }
-
-    delete[] triareas;
-
-    // Step 3: Filter walkable surfaces
-
-    // Step 4: Partitioning
-    rcCompactHeightfield* cheightfield = rcAllocCompactHeightfield();
-    if (!cheightfield) {
-        return false;
-    }
-    if (!rcBuildCompactHeightfield(context, config.walkableHeight, config.walkableClimb, *heightfield, *cheightfield)) {
-        return false;
-    }
-    rcFreeHeightField(heightfield);
-
-    if (!rcErodeWalkableArea(context, config.walkableRadius, *cheightfield)) {
-        return false;
-    }
-
-    // Use watershed partitioning
-    if (!rcBuildDistanceField(context, *cheightfield)) {
-        return false;
-    }
-
-    if (!rcBuildRegions(context, *cheightfield, 0, config.minRegionArea, config.mergeRegionArea)) {
-        return false;
-    }
-
-    // Step 5: Contours
-    rcContourSet* cset = rcAllocContourSet();
-    if (!cset) {
-        return false;
-    }
-    if (!rcBuildContours(context, *cheightfield, config.maxSimplificationError, config.maxEdgeLen, *cset)) {
-        return false;
-    }
-
-    // Step 6: poly mesh
-    rcPolyMesh* tileNavmesh;
-    tileNavmesh = rcAllocPolyMesh();
-    if (!tileNavmesh) {
-        return false;
-    }
-    if (!rcBuildPolyMesh(context, *cset, config.maxVertsPerPoly, *tileNavmesh)) {
-        return false;
-    }
-
-    overlappingTiles.push_back(tileNavmesh);
-
-    return true;
-}
-DllExport [[maybe_unused]] void ClearTiles() {
-    for (rcPolyMesh* mesh : overlappingTiles)
-        rcFreePolyMesh(mesh);
-    overlappingTiles.clear();
-}
-
-rcPolyMesh* mergedTiles = nullptr;
-DllExport [[maybe_unused]] bool MergeTiles() {
-    if (mergedTiles == nullptr) {
-        mergedTiles = rcAllocPolyMesh();
-    }
-
-    if (!rcMergePolyMeshes(context, overlappingTiles.data(), overlappingTiles.size(), *mergedTiles))
-        return false;
-
-    return true;
-}
-DllExport [[maybe_unused]] int GetMergedMeshMaxVertsPerPoly() {
-    if (mergedTiles != nullptr) {
-        return mergedTiles->nvp;
-    }
-    return -1;
-}
-DllExport [[maybe_unused]] int GetMergedMeshVertCount() {
-    if (mergedTiles != nullptr) {
-        return mergedTiles->nverts;
-    }
-    return -1;
-}
-DllExport [[maybe_unused]] int GetMergedMeshPolyCount() {
-    if (mergedTiles != nullptr) {
-        return mergedTiles->npolys;
-    }
-    return -1;
-}
-DllExport [[maybe_unused]] void GetMergedMeshVerts(void* buffer) {
-    if (mergedTiles != nullptr) {
-        memcpy(buffer, mergedTiles->verts, mergedTiles->nverts * sizeof(short) * 3);
-    }
-}
-DllExport [[maybe_unused]] void GetMergedMeshPolys(void* buffer) {
-    if (mergedTiles != nullptr) {
-        memcpy(buffer, mergedTiles->polys, mergedTiles->npolys * sizeof(short) * 2 * mergedTiles->nvp);
-    }
-}
-DllExport [[maybe_unused]] void GetMergedMeshAreas(void* buffer) {
-    if (mergedTiles != nullptr) {
-        memcpy(buffer, mergedTiles->areas, mergedTiles->npolys * sizeof(char));
-    }
-}
-DllExport [[maybe_unused]] void GetMergedMeshRegions(void* buffer) {
-    if (mergedTiles != nullptr) {
-        memcpy(buffer, mergedTiles->regs, mergedTiles->npolys * sizeof(short));
-    }
-}
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+rcContext *context = nullptr;
 
 rcPolyMesh *currentMesh = nullptr;
+
+rcConfig config;
 
 float bmin[3];
 float bmax[3];
 
-
+DllExport [[maybe_unused]] void
+SetNavmeshBuildParams(float cs, float ch, float walkableSlopeAngle, float walkableHeight, float walkableClimb,
+                      float walkableRadius, int minRegionArea) {
+    memset(&config, 0, sizeof(config));
+    config.cs = cs;
+    config.ch = ch;
+    config.walkableSlopeAngle = walkableSlopeAngle;
+    config.walkableHeight = (int) ceilf(walkableHeight / ch);
+    config.walkableClimb = (int) floorf(walkableClimb / ch);
+    config.walkableRadius = (int) ceilf(walkableRadius / cs);
+    config.maxEdgeLen = (int) 12;
+    config.maxSimplificationError = 1.3f;
+    config.minRegionArea = (int) rcSqr(minRegionArea); // Note: area = size*size
+    config.mergeRegionArea = (int) rcSqr(20); // Note: area = size*size
+    config.maxVertsPerPoly = 3;
+    config.detailSampleDist = 6;
+    config.detailSampleMaxError = 1;
+}
 
 // Navmesh building based off of demo:
 // https://github.com/recastnavigation/recastnavigation/blob/master/RecastDemo/Source/Sample_SoloMesh.cpp
